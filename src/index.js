5dbf3267447e582cfcb8bb2e8f33140f289422d9
Shapovalova-aal <prosto.isit@gmail.com>
2025-02-03 21:27:46 +0800

    47. CopyState in ProfileReducer

--- src/index.js
+++ src/index.js
@@ -7,22 +7,22 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-let reRenderEntireTree = (state) => {
-  root.render(
-    // <StoreContext.Provider value={store}>
-    <Provider store={store}>
-      <App
-        state={state}
-        //  store={store}
-      />
-    </Provider>
-    // </StoreContext.Provider>
-  );
-};
+// let reRenderEntireTree = (state) => {
+root.render(
+  // <StoreContext.Provider value={store}>
+  <Provider store={store}>
+    <App
+      state={store.getState()}
+      //  store={store}
+    />
+  </Provider>
+  // </StoreContext.Provider>
+);
+// };
 
-reRenderEntireTree(store.getState());
+// reRenderEntireTree(store.getState());
 
-store.subscribe(() => {
-  let state = store.getState();
-  reRenderEntireTree(state);
-});
+// store.subscribe(() => {
+//   let state = store.getState();
+//   reRenderEntireTree(state);
+// });
