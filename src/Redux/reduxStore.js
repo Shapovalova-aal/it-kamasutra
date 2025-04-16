eedb353ed671e71116a36dc8efffc3ab6fa23b28
Shapovalova-aal <prosto.isit@gmail.com>
2025-04-16 21:07:55 +0300

    75. redux-form #1(login)

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -5,13 +5,14 @@ import navbarReducer from "./navbarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import { thunk } from "redux-thunk";
-
+import { reducer as formReducer } from "redux-form";
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogReducer,
   navBar: navbarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunk));
--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,12 +1,61 @@
 import React from "react";
 import classes from "./Login.module.css";
+import ButtonGrey from "../../UI/button/ButtonGrey/ButtonGrey";
+import { Field, reduxForm } from "redux-form";
+
+const onSubmit = (formData) => {
+  console.log(formData);
+  // dispatch
+};
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field
+          type={"text"}
+          placeholder={"login"}
+          component={"input"}
+          name={"login"}
+        />
+      </div>
+      <div>
+        <Field
+          type={"password"}
+          placeholder={"password"}
+          component={"input"}
+          name={"password"}
+        />
+      </div>
+      <div className={classes.checkbox}>
+        <Field
+          type={"checkbox"}
+          component={"input"}
+          name={"rememberMe"}
+          id={"checkbox"}
+          className={classes.checkbox_input}
+        />
+        <label htmlFor={"checkbox"} className={classes.checkbox_label}>
+          Remember me
+        </label>
+      </div>
+      <div>
+        <button className={classes.btn}>submit</button>
+      </div>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm({
+  form: "login",
+})(LoginForm);
+
 const Login = () => {
   return (
     <div className={classes.container}>
       <div className={classes.form}>
         <div className={classes.login}>Login</div>
-        <input type="text" placeholder="login" />
-        <input type="password" placeholder="password" />
+        <LoginReduxForm onSubmit={onSubmit} />
       </div>
     </div>
   );
